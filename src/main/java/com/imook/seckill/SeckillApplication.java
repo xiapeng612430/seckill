2ed0213d0742c981db8e9cfe213860e5d4dd2aaf
xianpeng.xia <951256916>
2019-06-04 02:00:00 +0800

    实现dao

--- src/main/java/com/imook/seckill/SeckillApplication.java
+++ src/main/java/com/imook/seckill/SeckillApplication.java
@@ -1,13 +1,11 @@
 package com.imook.seckill;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class SeckillApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SeckillApplication.class, args);
+        //   SpringApplication.run(SeckillApplication.class, args);
     }
 
 }
