97c4decb2c25be4c2144857d27202740f13b09bb
xianpeng.xia <951256916>
2019-06-04 12:37:46 +0800

    声明式事务配置

--- src/main/java/com/imook/seckill/service/impl/SeckillServiceImpl.java
+++ src/main/java/com/imook/seckill/service/impl/SeckillServiceImpl.java
@@ -17,6 +17,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.DigestUtils;
 
 /**
@@ -63,6 +64,7 @@ public class SeckillServiceImpl implements SeckillService {
     }
 
     @Override
+    @Transactional
     public SeckillExecution executeSeckill(long seckillId, long userPhone, String md5) throws SeckillException, RepeatKillException, SeckillCloseException {
         if (md5 == null || md5.equals(geMd5(seckillId))) {
             throw new SeckillException("seckill data rewrite");
