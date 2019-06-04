8b7fd8813f9033c50c63eda85f1fab176cb66fa7
xianpeng.xia <951256916>
2019-06-04 13:20:02 +0800

    logback

--- src/main/java/com/imook/seckill/service/impl/SeckillServiceImpl.java
+++ src/main/java/com/imook/seckill/service/impl/SeckillServiceImpl.java
@@ -66,7 +66,7 @@ public class SeckillServiceImpl implements SeckillService {
     @Override
     @Transactional
     public SeckillExecution executeSeckill(long seckillId, long userPhone, String md5) throws SeckillException, RepeatKillException, SeckillCloseException {
-        if (md5 == null || md5.equals(geMd5(seckillId))) {
+        if (md5 == null || !md5.equals(geMd5(seckillId))) {
             throw new SeckillException("seckill data rewrite");
         }
         //执行秒杀逻辑：减库存+记录购买行为
---
+++ src/test/java/com/imook/seckill/service/SeckillServiceTest.java
@@ -0,0 +1,54 @@
+package com.imook.seckill.service;
+
+import com.imook.seckill.entity.Seckill;
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by xianpeng.xia
+ * on 2019-06-04 12:39
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/spring-dao.xml", "classpath:spring/spring-service.xml"})
+public class SeckillServiceTest {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    private SeckillService seckillService;
+
+    @Test
+    public void getSeckillList() throws Exception {
+        List<Seckill> list = seckillService.getSeckillList();
+        logger.info("list={}", list);
+    }
+
+    @Test
+    public void getById() throws Exception {
+        Seckill seckill = seckillService.getById(1000);
+        logger.info("seckill={}" + seckill);
+    }
+
+
+    /**
+     * 秒杀开启输出秒杀接口地址
+     * 否则输出系统时间和秒杀时间
+     */
+    @Test
+    public void exportSeckillUrl() throws Exception {
+
+    }
+
+    /**
+     * 执行秒杀
+     */
+    @Test
+    public void executeSeckillhrows() throws Exception {
+
+    }
+
+
+}
